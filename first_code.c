1228ff8ccd3d56004e66939107df1ce1cc0bc927
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-10-04 21:52:09 +0600

    hello c programming code run

---
+++ first_code.c
@@ -0,0 +1,18 @@
+// #include<stdio.h>
+
+// int main()
+// {
+//     printf("hello world\n");
+
+//     return 0;
+// }
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello world!!\n");
+    printf("Hello C Programming\n");
+
+    return 0;
+}
